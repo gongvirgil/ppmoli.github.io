68950eae7366df97662278df2821780f7ebdeadc
gongvirgil <gongvirgil@gmail.com>
2015-12-08 18:19:04 +0800

    change

--- public/js/post.js
+++ public/js/post.js
@@ -1,14 +1,9 @@
 $(document).ready(function(){
     //在文章中查找title并填充到div AnchorContent中
-	$(".post_content").find("h2,h3,h4,h5,h6").each(function(i,item){
+	$(".post-content").find("h1,h2,h3,h4,h5,h6").each(function(i,item){
 	    var tag = $(item).get(0).localName;
 	    $(item).attr("id","wow"+i);
 	    $("#AnchorContent").append('<li><a class="new'+tag+' anchor-link" onclick="return false;" href="#" link="#wow'+i+'">'/*+(i+1)+" · "*/+$(this).text()+'</a></li>');
-	    $(".newh2").css("margin-left",0);
-	    $(".newh3").css("margin-left",5);
-	    $(".newh4").css("margin-left",10);
-	    $(".newh5").css("margin-left",15);
-	    $(".newh6").css("margin-left",20);
 	});
 	$(".anchor-link").click(function(){
 	    $("html,body").animate({scrollTop: $($(this).attr("link")).offset().top}, 1000);
@@ -39,4 +34,32 @@ $(document).ready(function(){
 	    }
 	    $("#AnchorContent").toggle();
 	});
+
+
+	$('pre').each(function(index, el) {
+		$(this).addClass('c'+index);
+		$($('#tools-html').html()).insertBefore('pre:eq('+index+')').find('.copy').attr('tag', 'c'+index);
+		$($('#tools-html').html()).insertAfter('pre:eq('+index+')').find('.copy').attr('tag', 'c'+index);
+	});
+
+    function clipCallback(client) {
+        client.setText($.trim(this.innerHTML));
+        var pos = parent.position();
+        var tester = $('#tester');
+        tester.show().css({opacity:1,left:pos.left,top:pos.top}).stop().animate({
+            opacity: 0,
+            top: '-=20'
+         },function(){
+            $(this).hide();
+         });
+    }
+    $('.copy').each(function(){
+    	var ol = $('ol', $('pre.'+$(this).attr('tag')) );
+        var clip = new ZeroClipboard(ol, {moviePath: "/public/js/ZeroClipboard.swf"});
+        clip.on('mousedown', clipCallback);
+        
+        if(navigator.userAgent.indexOf('Firefox') !=-1) {
+            $(clip.htmlBridge).css('opacity',0);
+        }
+    });
 })
